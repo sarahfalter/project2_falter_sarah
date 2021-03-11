1c46aa3ef373a4e284d5849e682024396ae07a01
sarahfalter <sarah.falter@ufl.edu>
2021-03-11 08:10:36 -0600

    form editing, gallery started, homepage edits

---
+++ hamburger.js
@@ -0,0 +1,29 @@
+
+/*
+
+hamburger menu code pen
+https://codepen.io/g13nn/pen/eHGEF
+
+*/
+
+
+
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+$("#container").backstretch([
+  { width: 200, url: "images/portfolio5.png" },
+  { width: 200, url: "images/portfolio6.png" },
+  { width: 200, url: "images/portfolio3.jpg" },
+  { width: 200, url: "images/portfolio4.png" },
+], {duration: 4000, transition:"fade", transitionDuration: 1000});
