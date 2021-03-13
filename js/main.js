e53c6eabe007c9bf2f29d71e1136d024a94a2ae3
sarahfalter <sarah.falter@ufl.edu>
2021-03-13 10:16:08 -0600

    moved backstretch to homepage and fixed styling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 
-$("#container").backstretch([
-  { width: 600, url: "images/portfolio5.png" },
-  { width: 600, url: "images/portfolio6.png" },
-  { width: 600, url: "images/portfolio3.jpg" },
-  { width: 600, url: "images/portfolio4.png" },
+$('#content').backstretch([
+  { width: 400, url: "images/portfolio5.png" },
+  { width: 400, url: "images/portfolio6.png" },
+  { width: 400, url: "images/portfolio3.jpg" },
+  { width: 400, url: "images/portfolio4.png" },
 ], {duration: 4000, transition:"fade", transitionDuration: 1000});
